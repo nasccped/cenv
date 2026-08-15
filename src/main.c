638a16c39bd67c9c765f7d3eb8ff4060d784a173
nasccped <pedro.main.tec@gmail.com>
2026-08-15 16:18:36 +0000

    chore: init cli basics

--- src/main.c
+++ src/main.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
+#include <string.h>
+
+// Prints the help message
+void print_help();
+
+// Prints the app version.
+void print_version();
 
 int main(int argc, char *argv[]) {
-  printf("Welcome to c-env!\n");
+  char *subcommand;
+
+  switch (--argc) {
+
+    // when no args
+    case 0:
+      printf("Welcome to c-env!\n");
+      break;
+
+    // when one arg
+    case 1:
+      argv++;
+      subcommand = argv[0];
+
+      if (strcmp(subcommand, "help") == 0)
+        print_help();
+      else if (strcmp(subcommand, "version") == 0)
+        print_version();
+      else {
+        fprintf(stderr, "Non valid subcommand: %s.\n", argv[0]);
+        return 1;
+      }
+      break;
+
+    // when more than 1 arg
+    default:
+      fprintf(stderr, "Expected one (or no) subcommand. Got %d.\n", argc);
+      return 1;
+  }
+
   return 0;
+}
+
+void print_help() {
+  printf("TODO: add help printing!\n");
+}
+
+void print_version() {
+  printf("under development!\n");
 }
