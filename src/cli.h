de49d31430411f9720a6c3a4aa950cb56c06c00c
nasccped <pedro.main.tec@gmail.com>
2026-08-20 21:00:49 +0000

    chore: add action kind to detect cli program state

--- src/cli.h
+++ src/cli.h
@@ -4,7 +4,18 @@
 /* Refers to the cli structured call. This struct can be parsed from stdin args by using the
  * 'parse_cli' function. */
 typedef struct {
-  // TODO: add cli fields.
+
+  /* Kind of action being executed. */
+  enum {
+
+    /* Refers to the help flag at global scope (cenv --help). */
+    HELP_SUBCOMMAND_AS_FLAG,
+
+    /* Refers to the version flag at global scope (cenv --version). */
+    VERSION_SUBCOMMAND_AS_FLAG
+
+  } action_kind;
+
 } Cli;
 
 /* Takes a cli pointer and parse all the arguments into a final struct. It also returns an
