b5bf0c5a13118e0c82c1275a468888159137a78d
nasccped <pedro.main.tec@gmail.com>
2026-08-20 19:21:20 +0000

    feat: add string coloring support

--- src/cenvutils.c
+++ src/cenvutils.c
@@ -3,17 +3,66 @@
 #include <string.h>
 #include "cenvutils.h"
 
-/* Error tag string (being used on 'print_error_tag' function). */
-#define ERROR_TAG "\x1b[91merror\x1b[97m:\x1b[0m"
+/* Default string being used when string param (on 'colored_string_set_values') points to null. */
+#define COLORED_STRING_DEFAULT_STRING ""
 
-/* Note tag string (being used on 'print_error_tag' function). */
-#define NOTE_TAG "\x1b[96mnote\x1b[97m:\x1b[0m"
+/* Default 'ColoredString' data for double colon when printing tags. */
+static const ColoredString COLORED_STRING_DOUBLE_COLON_TAG = {
+  .stl = NONE_STYLE,
+  .fg = BRIGHT_WHITE,
+  .bg = NONE_COLOR,
+  .s = ":"
+};
+
+/* Default 'ColoredString' data for error tag.
+ * NOTE: It only refers to the 'error' string, not the double colon.  */
+static const ColoredString COLORED_STRING_ERROR_TAG = {
+  .stl = BOLD,
+  .fg = BRIGHT_RED,
+  .bg = NONE_COLOR,
+  .s = "error"
+};
+
+/* Default 'ColoredString' data for note tag.
+ * NOTE: It only refers to the 'note' string, not the double colon.  */
+static const ColoredString COLORED_STRING_NOTE_TAG = {
+  .stl = BOLD,
+  .fg = BRIGHT_CYAN,
+  .bg = NONE_COLOR,
+  .s = "note"
+};
+
+void colored_string_set_values(
+  ColoredString *cs,
+  AnsiStyleVariant stl,
+  AnsiColorVariant fg,
+  AnsiColorVariant bg,
+  char *s
+) {
+  if (!cs)
+    return;
+
+  cs->stl = stl;
+  cs->fg = fg;
+  cs->bg = bg;
+  cs->s = s ? s : COLORED_STRING_DEFAULT_STRING;
+}
 
 void print_error_tag(const char *restrict fmt, ...) {
+  char error_tag_string[32], double_colon_string[16];
   va_list args;
-  va_start(args, fmt);
 
-  fprintf(stderr, "%s ", ERROR_TAG);
+  va_start(args, fmt);
+  string_set_from_colored_string(
+    error_tag_string,
+    (ColoredString *) &COLORED_STRING_ERROR_TAG
+  );
+  string_set_from_colored_string(
+    double_colon_string,
+    (ColoredString *) &COLORED_STRING_DOUBLE_COLON_TAG
+  );
+
+  fprintf(stderr, "%s%s ", error_tag_string, double_colon_string);
   vfprintf(stderr, fmt, args);
 
   va_end(args);
@@ -21,10 +70,20 @@ void print_error_tag(const char *restrict fmt, ...) {
 
 void print_note_tag(int is_err, const char *restrict fmt, ...) {
   FILE *dest = is_err ? stderr : stdout;
+  char note_tag_string[32], double_colon_string[16];
   va_list args;
-  va_start(args, fmt);
 
-  fprintf(dest, "%s ", NOTE_TAG);
+  va_start(args, fmt);
+  string_set_from_colored_string(
+    note_tag_string,
+    (ColoredString *) &COLORED_STRING_NOTE_TAG
+  );
+  string_set_from_colored_string(
+    double_colon_string,
+    (ColoredString *) &COLORED_STRING_DOUBLE_COLON_TAG
+  );
+
+  fprintf(dest, "%s%s ", note_tag_string, double_colon_string);
   vfprintf(dest, fmt, args);
 
   va_end(args);
@@ -76,4 +135,44 @@ int string_is_whitespace(char *s) {
 
   // final instruction means only whitespace chars found
   return 1;
+}
+
+void string_set_from_colored_string(char *s, ColoredString *cs) {
+  // create temp string buffer (store ansi escape) and string buffer length.
+  char temp_string[16] = {0};
+  int tslen = 0;
+
+  if (!s || !cs)
+    return;
+
+  // if some style being used.
+  if (cs->stl != NONE_STYLE)
+    tslen += sprintf(temp_string, "\x1b[%d", cs->stl);
+
+  // if some foreground color being used.
+  if (cs->fg != NONE_COLOR) {
+    // if buffer still empty.
+    if (!tslen)
+      tslen += sprintf(temp_string, "\x1b[%d", cs->fg + 30);
+
+    // else, add fg param
+    else
+      tslen += sprintf(temp_string + tslen, ";%d", cs->fg + 30);
+  }
+
+  // if some background color being used.
+  if (cs->bg != NONE_COLOR) {
+    // if buffer still empty.
+    if (!tslen)
+      tslen += sprintf(temp_string, "\x1b[%d", cs->bg + 40);
+
+    // else, add fg param
+    else
+      tslen += sprintf(temp_string + tslen, ";%d", cs->bg + 40);
+  }
+
+  if (tslen)
+    sprintf(s, "%sm%s\x1b[0m", temp_string, cs->s);
+  else
+    sprintf(s, "%s", cs->s);
 }
