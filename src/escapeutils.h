7033cb7a7238212476873cb26b2239c7ff2e0cb3
nasccped <pedro.main.tec@gmail.com>
2026-08-20 21:02:39 +0000

    rem(escapeutils): no longer necessary

--- src/escapeutils.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ESCAPEUTILS_H_
-#define _ESCAPEUTILS_H_
-
-#define RED(STR)   "\x1b[91m" STR "\x1b[0m"
-#define GREEN(STR) "\x1b[92m" STR "\x1b[0m"
-#define WHITE(STR) "\x1b[97m" STR "\x1b[0m"
-
-#endif
