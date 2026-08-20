06d4bb04601fd1c586c0060ad55c9c2fcfde5a04
nasccped <pedro.main.tec@gmail.com>
2026-08-20 15:25:41 +0000

    chore: add printutils file doc

--- src/printutils.h
+++ src/printutils.h
@@ -1,5 +1,7 @@
 #ifndef _PRINTUTILS_H_
 #define _PRINTUTILS_H_
+/* This file defines printing utilities for the entire program. It isn't a big definition file.
+ * That's just a way to avoid printf and fprintf misuse when logging to final user. */
 
 /* Prints the formated string directly to stderr. */
 void print_err(const char *restrict fmt, ...);
