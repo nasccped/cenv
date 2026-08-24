b0b3f77e9650b3984f6d280d3d8459fc94f65537
nasccped <pedro.main.tec@gmail.com>
2026-08-24 18:19:40 +0000

    regression: use str literal macros instead of escape struct

--- src/cenvutils.c
+++ src/cenvutils.c
@@ -6,95 +6,35 @@
 /* Default string being used when string param (on 'colored_string_set_values') points to null. */
 #define COLORED_STRING_DEFAULT_STRING ""
 
-/* Default 'ColoredString' data for double colon when printing tags. */
-static const ColoredString COLORED_STRING_DOUBLE_COLON_TAG = {
-  .stl = NONE_STYLE,
-  .fg = BRIGHT_WHITE,
-  .bg = NONE_COLOR,
-  .s = ":"
-};
+/* Aux definition used within tag definitions. */
+#define WHITE_DOUBLE_COLON BRIGHT_WHITE_LITERAL(":")
 
-/* Default 'ColoredString' data for error tag.
- * NOTE: It only refers to the 'error' string, not the double colon.  */
-static const ColoredString COLORED_STRING_ERROR_TAG = {
-  .stl = BOLD,
-  .fg = BRIGHT_RED,
-  .bg = NONE_COLOR,
-  .s = "error"
-};
+/* Kind of tag when calling 'print_error_tag' function. */
+#define ERROR_TAG BOLD_BRIGHT_RED_LITERAL("error") WHITE_DOUBLE_COLON
 
-/* Default 'ColoredString' data for note tag.
- * NOTE: It only refers to the 'note' string, not the double colon.  */
-static const ColoredString COLORED_STRING_NOTE_TAG = {
-  .stl = BOLD,
-  .fg = BRIGHT_CYAN,
-  .bg = NONE_COLOR,
-  .s = "note"
-};
-
-void colored_string_set_values(
-  ColoredString *cs,
-  AnsiStyleVariant stl,
-  AnsiColorVariant fg,
-  AnsiColorVariant bg,
-  char *s
-) {
-  if (!cs)
-    return;
-
-  cs->stl = stl;
-  cs->fg = fg;
-  cs->bg = bg;
-  cs->s = s ? s : COLORED_STRING_DEFAULT_STRING;
-}
+/* Kind of tag when calling 'print_note_tag' function. */
+#define NOTE_TAG BOLD_BRIGHT_CYAN_LITERAL("note") WHITE_DOUBLE_COLON
 
 void print_error_tag(const char *restrict fmt, ...) {
-  char error_tag_string[32], double_colon_string[16];
   va_list args;
-
   va_start(args, fmt);
-  string_set_from_colored_string(
-    error_tag_string,
-    (ColoredString *) &COLORED_STRING_ERROR_TAG
-  );
-  string_set_from_colored_string(
-    double_colon_string,
-    (ColoredString *) &COLORED_STRING_DOUBLE_COLON_TAG
-  );
-
-  fprintf(stderr, "%s%s ", error_tag_string, double_colon_string);
+  fprintf(stderr, "%s ", ERROR_TAG);
   vfprintf(stderr, fmt, args);
-
   va_end(args);
 }
 
 void print_note_tag(int is_err, const char *restrict fmt, ...) {
-  FILE *dest = is_err ? stderr : stdout;
-  char note_tag_string[32], double_colon_string[16];
   va_list args;
-
   va_start(args, fmt);
-  string_set_from_colored_string(
-    note_tag_string,
-    (ColoredString *) &COLORED_STRING_NOTE_TAG
-  );
-  string_set_from_colored_string(
-    double_colon_string,
-    (ColoredString *) &COLORED_STRING_DOUBLE_COLON_TAG
-  );
-
-  fprintf(dest, "%s%s ", note_tag_string, double_colon_string);
-  vfprintf(dest, fmt, args);
-
+  fprintf(is_err ? stderr : stdout, "%s ", NOTE_TAG);
+  vfprintf(is_err ? stderr : stdout, fmt, args);
   va_end(args);
 }
 
 void printerr(const char *restrict fmt, ...) {
   va_list args;
   va_start(args, fmt);
-
   vfprintf(stderr, fmt, args);
-
   va_end(args);
 }
 
@@ -135,49 +75,4 @@ int string_is_whitespace(char *s) {
 
   // final instruction means only whitespace chars found
   return 1;
-}
-
-void string_set_from_colored_string(char *s, ColoredString *cs) {
-  // create temp string buffer (store ansi escape) and string buffer length.
-  char temp_string[16] = {0};
-  int tslen = 0;
-
-  if (!s || !cs)
-    return;
-
-  // if some style being used.
-  if (cs->stl != NONE_STYLE)
-    tslen += sprintf(temp_string, "\x1b[%d", cs->stl);
-
-  // if some foreground color being used.
-  if (cs->fg != NONE_COLOR) {
-    // if buffer still empty.
-    if (!tslen)
-      tslen += sprintf(temp_string, "\x1b[%d", cs->fg + 30);
-
-    // else, add fg param
-    else
-      tslen += sprintf(temp_string + tslen, ";%d", cs->fg + 30);
-  }
-
-  // if some background color being used.
-  if (cs->bg != NONE_COLOR) {
-    // if buffer still empty.
-    if (!tslen)
-      tslen += sprintf(temp_string, "\x1b[%d", cs->bg + 40);
-
-    // else, add fg param
-    else
-      tslen += sprintf(temp_string + tslen, ";%d", cs->bg + 40);
-  }
-
-  /* copy inner string. This allows 'ColoredString.s' holds a pointer to the self 's' string and
-   * don't panic when setting a new value to s when using itselfs pointer. */
-  char inner_string[1024] = {0};
-  strncpy(inner_string, cs->s, 1024);
-
-  if (tslen)
-    sprintf(s, "%sm%s\x1b[0m", temp_string, inner_string);
-  else
-    sprintf(s, "%s", inner_string);
 }
--- src/cenvutils.h
+++ src/cenvutils.h
@@ -3,70 +3,17 @@
 /* This file defines common utilities for cenv runtime, such as print_erro_tag, string mode
  * checkers and so on... */
 
-/* Refers to all available ansi color escapes. They're tagged if it's respective integer value
- * (which must be handled based on the put qualifier (foreground/background)). */
-typedef enum {
+/* Applies bold escape sequence for string literals (ONLY). */
+#define BOLD_LITERAL(STR) "\x1b[1m" STR "\x1b[0m"
 
-  // means no color
-  NONE_COLOR = -1,
+/* Applies bold + bright red escape sequence for string literals (ONLY). */
+#define BOLD_BRIGHT_RED_LITERAL(STR) "\x1b[1;91m" STR "\x1b[0m"
 
-  // normal colors
-  BLACK   = 0,
-  RED     = 1,
-  GREEN   = 2,
-  YELLOW  = 3,
-  BLUE    = 4,
-  MAGENTA = 5,
-  CYAN    = 6,
-  WHITE   = 7,
+/* Applies bold + bright cyan escape sequence for string literals (ONLY). */
+#define BOLD_BRIGHT_CYAN_LITERAL(STR) "\x1b[1;96m" STR "\x1b[0m"
 
-  // bright colors.
-  BRIGHT_BLACK   = 60,
-  BRIGHT_RED     = 61,
-  BRIGHT_GREEN   = 62,
-  BRIGHT_YELLOW  = 63,
-  BRIGHT_BLUE    = 64,
-  BRIGHT_MAGENTA = 65,
-  BRIGHT_CYAN    = 66,
-  BRIGHT_WHITE   = 67
-
-} AnsiColorVariant;
-
-/* Refers to available ansi format styles. */
-typedef enum {
-  NONE_STYLE = -1,
-  BOLD       = 1,
-  ITALIC     = 3
-} AnsiStyleVariant;
-
-/* Refers to an ansi colored string. */
-typedef struct {
-
-  /* Style being used, such as bold, italic and so on... */
-  AnsiStyleVariant stl;
-
-  
-  AnsiColorVariant
-    /* Color variant being used on foreground. */
-    fg,
-    /* Color variant being used on background. */
-    bg;
-
-  /* String pointer being held. */
-  char *s;
-
-} ColoredString;
-
-/* Set new values on a 'ColoredString' pointer. Note that this function only works if 'cs' points
- * to a non NULL address. 'string' pointer can also be null. If so, 'ColoredString.s' field gonna
- * point to an empty string. */
-void colored_string_set_values(
-  ColoredString *cs,
-  AnsiStyleVariant stl,
-  AnsiColorVariant fg,
-  AnsiColorVariant bg,
-  char *s
-);
+/* Applies bright white escape sequence for string literals (ONLY). */
+#define BRIGHT_WHITE_LITERAL(STR) "\x1b[97m" STR "\x1b[0m"
 
 /* Print an error tag to stderr (automatically). Note that this function doesn't automatically adds
  * newline at the end. You must handle it manually! */
@@ -86,9 +33,5 @@ int string_is_program_flag(char *s);
 
 /* Returns if the provided char pointer refers to NULL / zero-length / whitespace only string. */
 int string_is_whitespace(char *s);
-
-/* Sets 's' string inner value based on cs fields. Note that this function doesn't run overflow
- * checking. Just be sure about string mem size. */
-void string_set_from_colored_string(char *s, ColoredString *cs);
 
 #endif
